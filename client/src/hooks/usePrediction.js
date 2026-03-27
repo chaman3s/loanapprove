bbaa6606431b691d0b730ef4f110c85ccf632fd9
chaman3s <chamanggarwal@gmail.com>
2026-03-27 14:52:33 +0530

    change api url

--- client/src/hooks/usePrediction.js
+++ client/src/hooks/usePrediction.js
@@ -9,7 +9,7 @@ export function usePrediction() {
     setError(null);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/v1/users/predict", {
+      const res = await fetch("https://loanapprove.onrender.com/api/v1/users/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
